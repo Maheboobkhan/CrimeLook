35bf35690dc4db5450b88c4adb96c84d120964d4
Maheboobkhan <maheboobkhan18202@gmail.com>
2023-12-26 14:33:22 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import "./App.css"
 import MainPage from './components/MainPage/MainPage';
 import MagazineGallery from './components/MagazineGallery/MagazineGallery';
@@ -8,11 +8,12 @@ import AboutUs from './components/AboutUS/AboutUs';
 const App = () => {
   return (
     <div className='App'>
+      <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/magazines' element={<MagazineGallery />} />
         <Route path='/about' element={<AboutUs />} />
-      </Routes>
+      </Routes></BrowserRouter>
     </div>
   )
 }

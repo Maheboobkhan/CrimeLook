46341d07d1232d88b9cf85b017c03ff0445ec074
Maheboobkhan <maheboobkhan18202@gmail.com>
2023-12-31 15:41:19 +0530

    final

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,62 +1,73 @@
-// components/LoginPage.js
-
-import React, { useEffect, useState } from 'react';
-import '../Login/Login.css'; // Importing the CSS file for styling
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import React, { useState } from 'react';
+import '../Login/Login.css';
+import Navbar from "../MainPage/Navbar/Navbar";
+import Footer from "../MainPage/Footer/Footer";
+import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
-    useEffect(()=>{
-        AOS.init({duration: 1000, once:true})
-    },[])
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Here you can handle the login logic
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
 
-  const handleReset = () => {
-    setUsername('');
-    setPassword('');
-  };
+    const handleReset = () => {
+        setUsername('');
+        setPassword('');
+    };
 
-  return (
-    <div className="login-container" data-aos="zoom-in">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    return (
+        <div>
+            <Navbar />
+            <div className="login-container">
+                <h2>Login</h2>
+                <div className="underline"></div>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="username">
+                            <FaUser className="fa-user" />
+                            Username:
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="Enter your username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">
+                            <FaLock className='fa-lock' />
+                            Password:
+                        </label>
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            id="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <span onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <i className="fas fa-eye-slash"></i> : <i className="fas fa-eye"></i>}
+                        </span>
+                    </div>
+                    <p className="forgot-password">Forgot password?</p>
+                    <div className="button-group">
+                        <button type="submit">Submit</button>
+                        <button type="button" onClick={handleReset}>Reset</button>
+                    </div>
+                </form>
+            </div>
+            <Footer />
         </div>
-
-        <div className="input-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="button-group">
-          <button type="submit">Submit</button>
-          <button type="button" onClick={handleReset}>Reset</button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Login;
